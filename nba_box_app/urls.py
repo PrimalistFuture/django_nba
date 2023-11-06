43ad22914f2b8318d1a0262a5363c7275cfab953
Vaughn Seekamp <vseekamp@gmail.com>
2023-11-06 10:06:28 -0800

    views without players

--- nba_box_app/urls.py
+++ nba_box_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/standings/', views.standings, name='standings'),
+    path('/<str:team>/', views.team, name='team'),
+    # path('/<str:team>/<str:player>/', views.player, name='player'),
 ]
--- nba_box_app/views.py
+++ nba_box_app/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-
-
 def index(request):
-    return HttpResponse('Hello World, you are at the nba_box index')
+    return HttpResponse('Hello World, you are at the nba_box index, and should see all of the games played yesterday.')
+
+def standings(request):
+    return HttpResponse('This will be where overall standing are located.')
+
+def team(request, team):
+    response = 'You are looking at %s games from the past week.'
+    return HttpResponse(response % team)
+
+# def player(request, team, player):
+#     return HttpResponse('You are looking at  %s of game details from the past week.' % player and team)
